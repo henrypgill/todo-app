00c3f55f5696596edcd7ba29194839a100300941
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-08-03 16:13:20 +0100

    now uses immer reducer, +upgraded functionality

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,5 @@
-
 import { Action } from "./App";
-import {useState} from "react"
-
+import { useState } from "react";
 
 export interface ToDoData {
     id: number;
@@ -17,60 +15,124 @@ export interface ToDoProps {
     dispatch(action: Action): void;
 }
 
-export function ToDo({todo, dispatch}: ToDoProps): JSX.Element {
+export function ToDo({ todo, dispatch }: ToDoProps): JSX.Element {
+    const [title, setTitle] = useState<string>(todo.title);
+    const [description, setDescription] = useState<string>(todo.description);
+    const [editingTitle, setEditingTitle] = useState<boolean>(false);
+    const [editingDescription, setEditingDescription] =
+        useState<boolean>(false);
+    const [showing, setShowing] = useState<boolean>(false);
 
-    const [title, setTitle] = useState<string>(todo.title)
-    const [description, setDescription] = useState<string>(todo.description)
-
-
-    function handleTitleChange(val: string){
-        setTitle(val)
+    function handleTitleChange(val: string) {
+        setTitle(val);
     }
-    function handleDescriptionChange(val: string){
+    function handleDescriptionChange(val: string) {
         setDescription(val);
     }
 
-    function handleDelete(){
+    function handleDelete() {
         const action: Action = {
             id: todo.id,
             type: "delete",
             value: todo,
-        }
-        dispatch(action)
+        };
+        dispatch(action);
     }
-    function handleComplete(){
+    function handleComplete() {
         const action: Action = {
             id: todo.id,
             type: "complete",
-            value: {...todo, status: "complete"},
-        }
-        dispatch(action)
+            value: { ...todo, status: "complete" },
+        };
+        dispatch(action);
     }
-    function handleSave(){
+    function handleIncomplete() {
         const action: Action = {
             id: todo.id,
-            type: "update",
-            value: {...todo, title: title, description: description},
-        }
-        dispatch(action)
+            type: "incomplete",
+            value: { ...todo, status: "incomplete" },
+        };
+        dispatch(action);
+    }
+    function handleSaveTitle() {
+        const action: Action = {
+            id: todo.id,
+            type: "update-title",
+            value: { ...todo, title: title, description: description },
+        };
+        dispatch(action);
+        setEditingTitle(false);
+    }
+    function handleSaveDescription() {
+        const action: Action = {
+            id: todo.id,
+            type: "update-description",
+            value: { ...todo, title: title, description: description },
+        };
+        dispatch(action);
+        setEditingDescription(false);
     }
 
-    // if (!editing) {
+    function handleToggle() {
+        showing ? setShowing(false) : setShowing(true);
+    }
+
     return (
         <div className="todo-card">
-            <input value={title} onChange={(e) => handleTitleChange(e.target.value)} className="todo-title"></input>
-            <input value={description} onChange={(e => handleDescriptionChange(e.target.value))} className="todo-description"></input>
-            <h3>{todo.status}</h3>
-            <div>
-                <button onClick={handleDelete} className="todo-button">delete</button>
-                <button onClick={handleSave} className="todo-button">save</button>
-                {todo.status !== "complete" && <button onClick={handleComplete} className="todo-button">complete</button>}
+            <div className="title-and-button-container">
+                {editingTitle ? (
+                    <input
+                        value={title}
+                        onChange={(e) => handleTitleChange(e.target.value)}
+                        className="todo-title"
+                        onBlur={handleSaveTitle}
+                    ></input>
+                ) : (
+                    <h3 onDoubleClick={() => setEditingTitle(true)}>{title}</h3>
+                )}
+                <button onClick={handleToggle}>{showing ? "-" : "+"}</button>
             </div>
+            {showing && (
+                <div className="todo-info">
+                    {editingDescription ? (
+                        <input
+                            value={description}
+                            onChange={(e) =>
+                                handleDescriptionChange(e.target.value)
+                            }
+                            className="todo-description"
+                            onBlur={handleSaveDescription}
+                        ></input>
+                    ) : (
+                        <p onDoubleClick={() => setEditingDescription(true)}>
+                            {description}
+                        </p>
+                    )}
+                    <h4>{todo.status}</h4>
+                    <div>
+                        <button onClick={handleDelete} className="todo-button">
+                            delete
+                        </button>
+                        {todo.status === "incomplete" ? (
+                            <button
+                                onClick={handleComplete}
+                                className="todo-button"
+                            >
+                                mark as complete
+                            </button>
+                        ) : (
+                            <button
+                                onClick={handleIncomplete}
+                                className="todo-button"
+                            >
+                                mark as incomplete
+                            </button>
+                        )}
+                    </div>
+                </div>
+            )}
         </div>
     );
-// } else {
-//     return (
-//         <input value={headingText} className="new-procedure-heading-input" onChange={(e) => logNchange(e.target.value)} onBlur={(e) => headingFocusLeave(e.target.value)}></input>
-//     );
-// }
+
+    // onKeyUpCapture={(e) => e.key === "Enter" ? setEditingDescription(false): null}
 }
