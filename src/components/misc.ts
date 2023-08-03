62bb5d8336af470af1c2c7d624f9147730be608e
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-08-03 16:13:59 +0100

    now fetches initial data and updates server data

--- src/components/misc.ts
+++ src/components/misc.ts
@@ -1,13 +1,12 @@
-    // console.log(importData)
+// console.log(importData)
 
-    // const initialData: ToDoData[] = importData
-    // const emptyToDo: ToDoData = {
-    //     id: -1,
-    //     title: "To-Do: Title",
-    //     description: "To-Do: Description",
-    //     created: "",
-    //     due: "",
-    //     status: "incomplete",
+// const initialData: ToDoData[] = importData
+// const emptyToDo: ToDoData = {
+//     id: -1,
+//     title: "To-Do: Title",
+//     description: "To-Do: Description",
+//     created: "",
+//     due: "",
+//     status: "incomplete",
 
-    // }
-    
+// }
